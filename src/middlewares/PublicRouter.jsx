dacfc8c31a0162b469a0272303283d7217677bbf
touhidun-isharify <49831775+touhi13@users.noreply.github.com>
2024-05-27 01:58:06 +0600

    Update PublicRouter.jsx

--- src/middlewares/PublicRouter.jsx
+++ src/middlewares/PublicRouter.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
-import { useSelector } from 'react-redux';
 
 const PublicRouter = ({ children }) => {
     const isLoggedIn = useAuth();
